2bd639008e94c643f95cdec5fccd4d50d5dee642
Kewal Kalsi <kkalsi7@gatech.edu>
2022-04-02 23:16:50 -0400

    update background.js with proper initial query

--- background.js
+++ background.js
@@ -3,12 +3,46 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (changeInfo.status == 'complete') {
         console.log(tab.url);
         if (tab.url.indexOf('amazon') != -1) {
-            console.log('Extension Enabled');
-            chrome.action.enable(tabId);
-            if (tab.url.indexOf('crid') != -1) {
-                chrome.scripting.executeScript({
-                    
-                });
+            if (tab.url.indexOf('dp') != -1) {
+                var baseUrl = "https://api.rainforestapi.com/request"
+                var myUrl = new URL(baseUrl);
+                myUrl.searchParams.append('api_key', '14E49F1A64804E339E2085E1FEC35DDB');
+                myUrl.searchParams.append('type', 'product');
+                myUrl.searchParams.append('url', tab.url);
+                fetch(myUrl)
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data)
+                    console.log(data.request_info.credits_remaining)
+                    console.log(data.product.brand);
+                    if (true == true) {
+                        var username = '01c3a0fa839a03e7082f7fd75562a339';
+                        var password = '616e68cca5bd5e8fbff19f99e12bc8bb'
+                        $.ajax({
+                            type: "GET",
+                            url: "myapi.php",
+                            dataType: 'json',
+                            headers: {
+                              "Authorization": "Basic " + btoa(username + ":" + password)
+                            },
+                            success: function (result){
+                                console.log(result)
+                            }
+                          });
+
+                        /*fetch('https://www.amee.com/api/companies?from_lat_lon=51.623,-0.0732&distance=20', {
+                            headers: {
+                                'Authorization': `Basic ${btoa(username)}:${btoa(password)}`,
+                            }
+                        })
+                        .then(response => response.json())
+                        .then(data => {
+                            jsonData = data;
+                            console.log(data);
+                        })*/
+                    }
+                })
+                .catch(error => console.error(error));
             }
         } else {
             console.log('Extension Disabled');
@@ -16,3 +50,17 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
         }
     }
 })
+
+function searchForItem() {
+    console.log("peepeepoopoo")
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => {
+        //var obj = JSON.parse(response)
+        //console.log(obj)
+        console.log("ASOIDUGAISKDGFALS");
+        return response.json();
+    })
+    .then(users => {
+        console.log(users);
+    })
+}
